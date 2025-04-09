78646fda8d34ee0b439751165cb40854cfa31b02
MianAtique <ateeqadrees@fissionmonster.com>
2025-04-09 14:03:53 +0500

    bug fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -23,6 +23,7 @@ class Cors
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', '*')
                 ->header('Access-Control-Allow-Credentials', true)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
                 ->header('Accept', 'application/json');
         } else {
