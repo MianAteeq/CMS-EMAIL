cb760a8ad7b0bc14f27046bc98ea6057cff02978
MianAtique <ateeqadrees@fissionmonster.com>
2025-04-10 10:44:39 +0500

    bug fix

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,12 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => ['https://main.d1pezhudgaxkse.amplifyapp.com'],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
