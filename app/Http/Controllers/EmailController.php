dd7a21d825065c183c974ea0d156b0787d101bd5
MianAtique <ateeqadrees@fissionmonster.com>
2025-08-05 20:26:35 +0500

    bug fix

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -21,6 +21,8 @@ class EmailController extends Controller
     public function getEmail(Request $request)
     {
 
+        return  DB::table('jobs')->delete();
+
         $details = [
             'email' => 'ateeqadrees83@gmail.com',
             'title' => 'Test Subject',
